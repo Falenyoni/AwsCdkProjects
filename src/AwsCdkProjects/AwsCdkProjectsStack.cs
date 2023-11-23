6ea7f679e67a9362c93aa1c965883b74b975de94
Bongani Nyoni <Bongani.Nyoni@kaha.co.za>
2023-11-23 10:50:39 +0200

    Initial commit

---
+++ src/AwsCdkProjects/AwsCdkProjectsStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace AwsCdkProjects
+{
+    public class AwsCdkProjectsStack : Stack
+    {
+        internal AwsCdkProjectsStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
